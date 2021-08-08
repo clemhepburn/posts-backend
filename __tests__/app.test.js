795d7767cbed3cc4e68aa359fcbe64f2fff5ef4b
Clem Hepburn <clemhepburn@gmail.com>
2021-08-08 14:17:12 -0700

    API is functioning

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,36 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
-  beforeEach(() => {
-    return setup(pool);
+describe('API routes', () => {
+
+  const agent = request.agent(app);
+
+  beforeAll(async () => {
+    await setup(pool);
   });
+
+  it('posts a post to the API via POST', async () => {
+    const res = await agent.post('/api/V1/posts')
+      .send({
+        id: '1',
+        name: 'agnes',
+        post: 'hello i am agnes'
+      });
+    expect(res.status).toBe(200);
+    expect(res.body.id).toBe('1');
+    expect(res.body.name).toBe('agnes');
+    expect(res.body.post).toBe('hello i am agnes');
+  });
+
+  it('gets posts from the API via GET', async () => {
+    const res = await agent.get('/api/V1/posts')
+      .query({
+        id: '1'
+      });
+    expect(res.status).toBe(200);
+    expect(res.body[0].id).toBe('1');
+    expect(res.body[0].name).toBe('agnes');
+    expect(res.body[0].post).toBe('hello i am agnes');
+  }
+  );
 });
